2147d036edc2f32122d0f89e7da6110b89c9e26b
Alex <horrorinls@mail.ru>
2023-09-03 21:20:17 +0600

    modified reset btn

--- js/order-form.js
+++ js/order-form.js
@@ -4,6 +4,7 @@ function orderForm() {
   var modal = document.getElementById('myModal');
   var span = document.getElementsByClassName('close')[0];
   const modalText = document.querySelector('#modal-text');
+  const resetBtn = document.querySelector('#reset');
 
   span.onclick = function () {
     modal.style.display = 'none';
@@ -15,9 +16,18 @@ function orderForm() {
     }
   };
 
+  resetBtn.addEventListener('click', () => {
+    myForm.elements.name.style.borderColor = 'transparent';
+    myForm.elements.phone.style.borderColor = 'transparent';
+    myForm.elements.comment.style.borderColor = 'transparent';
+  });
+
   submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     modal.classList.remove('modal-red');
+    myForm.elements.name.style.borderColor = 'transparent';
+    myForm.elements.phone.style.borderColor = 'transparent';
+    myForm.elements.comment.style.borderColor = 'transparent';
 
     const data = {
       name: myForm.elements.name.value,
@@ -57,16 +67,21 @@ function orderForm() {
 
     if (!myForm.elements.name.value) {
       isValid = false;
+      myForm.elements.name.style.borderColor = 'red';
       console.log('поле имя должно быть заполнено');
     }
 
     if (!myForm.elements.phone.value) {
       isValid = false;
+      myForm.elements.phone.style.borderColor = 'red';
+
       console.log('поле телефон должно быть заполнено');
     }
 
     if (!myForm.elements.comment.value) {
       isValid = false;
+      myForm.elements.comment.style.borderColor = 'red';
+
       console.log('обязательно напишите комментарий!');
     }
 
